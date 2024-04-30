c63d6a6f2a9cc2cb1514643b3413002f7626c9ba
jgenard <jeremygenard@outlook.com>
2024-04-30 13:24:24 +0200

    Exercice variables 101

---
+++ Variables101/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Exercices : Inversion de variables");
+
+Console.WriteLine("Valeurs initiales : ");
+int a = 1;
+int b = 3;
+
+Console.WriteLine($"a : {a}");
+Console.WriteLine($"b : {b}");
+
+// Inverser les valeurs de a et b.
+
+Console.WriteLine("Résultat : ");
+Console.WriteLine($"a : {a}");
+Console.WriteLine($"b : {b}");
