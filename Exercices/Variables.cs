743a9fe63067e776a31df4afeeed27e67b04c426
jgenard <jeremygenard@outlook.com>
2024-05-01 18:38:21 +0200

    Add exercise on variables

---
+++ Exercices/Variables.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercices;
+
+using FluentAssertions;
+
+public class Variables
+{
+    [Fact]
+    public void InversionDeVariables()
+    {
+        // ================================ CONSIGNES =================================
+        // Inverser les valeurs de a et b.
+        int a = 5;
+        int b = 2;
+        // ================================ VOTRE CODE ================================
+
+
+
+        // ================================ CONTROLES =================================
+        a.Should().Be(2);
+        b.Should().Be(5);
+    }
+}
